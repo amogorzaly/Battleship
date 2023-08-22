3a57d440bf57d95a345ec332d0a1539b3ec4a0a4
amogorzaly <amogorzaly1@gmail.com>
2023-08-22 11:27:53 +0200

    Cleancode

--- Game/BattleshipGame.cs
+++ Game/BattleshipGame.cs
@@ -1,82 +1,77 @@
 ﻿using Battleship.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Battleship.Logic
+namespace Battleship.Game
 {
-    internal class BattleshipLogic
+    internal class BattleshipGame
     {
-        private Random random;
-        private int numberOfFiveSquaresShips;
-        private int numberOfFourSquaresShips;
-        private int numberOfColumns;
-        private int numberOfRows;
-        private List<Tuple<char, int>> firedPositions;
-        private List<Ship> createdShips;
-        public BattleshipLogic(int numberOfFiveSquaresShips, int numberOfFourSquaresShips, int numberOfRows, int numberOfColumns,
+        private readonly Random _random;
+        private readonly int _numberOfFiveSquaresShips;
+        private readonly int _numberOfFourSquaresShips;
+        private readonly int _numberOfColumns;
+        private readonly int _numberOfRows;
+        private readonly List<Tuple<char, int>> _firedPositions;
+        private readonly List<Ship> _createdShips;
+        public BattleshipGame(int numberOfFiveSquaresShips, int numberOfFourSquaresShips, int numberOfRows, int numberOfColumns,
             ref List<Tuple<char, int>> firedPositions, ref List<Ship> createdShips)
         {
-            random = new Random();
-            this.numberOfFiveSquaresShips = numberOfFiveSquaresShips;
-            this.numberOfFourSquaresShips = numberOfFourSquaresShips;
-            this.numberOfRows = numberOfRows;
-            this.numberOfColumns = numberOfColumns;
-            this.firedPositions = firedPositions;
-            this.createdShips = createdShips;
+            _random = new Random();
+            this._numberOfFiveSquaresShips = numberOfFiveSquaresShips;
+            this._numberOfFourSquaresShips = numberOfFourSquaresShips;
+            this._numberOfRows = numberOfRows;
+            this._numberOfColumns = numberOfColumns;
+            this._firedPositions = firedPositions;
+            this._createdShips = createdShips;
         }
         public void Init(ref List<Ship> createdShips)
         {
-            for (int i = 0; i < numberOfFiveSquaresShips; i++)
+            for (int i = 0; i < _numberOfFiveSquaresShips; i++)
             {
-                bool hasShipBeenPlaced = false;
+                bool hasShipBeenPlaced;
                 do
                 {
-                    hasShipBeenPlaced = placeShip(5, ref createdShips);
+                    hasShipBeenPlaced = PlaceShip(5, ref createdShips);
                 }
                 while (hasShipBeenPlaced == false);
             }
-            for (int i = 0; i < numberOfFourSquaresShips; i++)
+            for (int i = 0; i < _numberOfFourSquaresShips; i++)
             {
-                bool hasShipBeenPlaced = false;
+                bool hasShipBeenPlaced;
                 do
                 {
-                    hasShipBeenPlaced = placeShip(4, ref createdShips);
+                    hasShipBeenPlaced = PlaceShip(4, ref createdShips);
                 }
                 while (hasShipBeenPlaced == false);
             }
         }
-        bool placeShip(int numberOfSquares, ref List<Ship> createdShips)
+
+        private bool PlaceShip(int numberOfSquares, ref List<Ship> createdShips)
         {
             bool wasShipPlaced = false;
-            int initialPosition = 0;
+            int initialPosition;
             Ship newShip = new Ship();
-            bool isHorizontal = Convert.ToBoolean(random.Next(2));
+            bool isHorizontal = Convert.ToBoolean(_random.Next(2));
             if (isHorizontal)//horizontal
             {
-                int rowNumber = random.Next(numberOfRows);
-                initialPosition = random.Next(numberOfColumns - numberOfSquares + 1);
+                int rowNumber = _random.Next(_numberOfRows);
+                initialPosition = _random.Next(_numberOfColumns - numberOfSquares + 1);
                 for (int j = initialPosition; j < numberOfSquares + initialPosition; j++)
                 {
-                    if (createdShips.Any(x => x.Coordinates.Any(x => x.Item1 == Convert.ToChar(65 + j) && x.Item2 == rowNumber + 1)))
+                    if (createdShips.Any(ship => ship.Coordinates.Any(coordinate => coordinate.Item1 == Convert.ToChar(65 + j) && coordinate.Item2 == rowNumber + 1)))
                     {
                         return false;
                     }
-                    string dd = Convert.ToChar(65 + j) + Convert.ToString(rowNumber + 1);
                     newShip.Coordinates.Add(new Tuple<char, int>(Convert.ToChar(65 + j), rowNumber + 1));
                     wasShipPlaced = true;
                 }
             }
             else //vertical
             {
-                int columnNumber = random.Next(numberOfColumns);
+                int columnNumber = _random.Next(_numberOfColumns);
                 char column = Convert.ToChar(65 + columnNumber);
-                initialPosition = random.Next(numberOfRows - numberOfSquares + 1);
+                initialPosition = _random.Next(_numberOfRows - numberOfSquares + 1);
                 for (int i = initialPosition; i < numberOfSquares + initialPosition; i++)
                 {
-                    if (createdShips.Any(x => x.Coordinates.Any(x => x.Item1 == column && x.Item2 == i + 1)))
+                    if (createdShips.Any(ship => ship.Coordinates.Any(coordinate => coordinate.Item1 == column && coordinate.Item2 == i + 1)))
                     {
                         return false;
                     }
@@ -87,33 +82,34 @@ namespace Battleship.Logic
             createdShips.Add(newShip);
             return wasShipPlaced;
         }
-        public void drawBattlefield(ref List<Ship> createdShips)
+
+        private void DrawBattlefield(ref List<Ship> createdShips)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("\t");
-            for (int j = 0; j < numberOfColumns; j++)
+            for (int j = 0; j < _numberOfColumns; j++)
             {
                 Console.Write(Convert.ToChar(65 + j) + " ");
             }
             Console.WriteLine();
-            for (int i = 0; i < numberOfRows; i++)
+            for (int i = 0; i < _numberOfRows; i++)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write(Convert.ToString(i + 1) + "\t");
                 Console.ForegroundColor = ConsoleColor.White;
-                for (int j = 0; j < numberOfColumns; j++)
+                for (int j = 0; j < _numberOfColumns; j++)
                 {
-                    if (!firedPositions.Any(x => x.Item1 == Convert.ToChar(65 + j) && x.Item2 == i + 1))
+                    if (!_firedPositions.Any(fired => fired.Item1 == Convert.ToChar(65 + j) && fired.Item2 == i + 1))
                     {
                         Console.BackgroundColor = ConsoleColor.Gray;
                         Console.Write("  ");
                     }
-                    else if (createdShips.Any(x => !x.IsSunk && x.Coordinates.Any(x => x.Item1 == Convert.ToChar(65 + j) && x.Item2 == i + 1)))
+                    else if (createdShips.Any(ship => !ship.IsSunk && ship.Coordinates.Any(coordinate => coordinate.Item1 == Convert.ToChar(65 + j) && coordinate.Item2 == i + 1)))
                     {
                         Console.BackgroundColor = ConsoleColor.Green;
                         Console.Write("X ");
                     }
-                    else if (createdShips.Any(x => x.IsSunk && x.Coordinates.Any(x => x.Item1 == Convert.ToChar(65 + j) && x.Item2 == i + 1)))
+                    else if (createdShips.Any(ship => ship.IsSunk && ship.Coordinates.Any(coordinate => coordinate.Item1 == Convert.ToChar(65 + j) && coordinate.Item2 == i + 1)))
                     {
                         Console.BackgroundColor = ConsoleColor.Green;
                         Console.ForegroundColor = ConsoleColor.Red;
@@ -136,58 +132,60 @@ namespace Battleship.Logic
         {
             do
             {
-                drawBattlefield(ref createdShips);
+                DrawBattlefield(ref createdShips);
                 Console.Write("Enter target coordinates (e.g. A1): ");
-                string typedCoordinates = Console.ReadLine();
-                if (!isTypedCoordinateValid(typedCoordinates))
+                string? typedCoordinates = Console.ReadLine();
+                if (!IsTypedCoordinateValid(typedCoordinates))
                 {
                     Console.WriteLine("Invalid coordinates!");
                     continue;
                 }
-                Int32.TryParse(typedCoordinates.Substring(1), out int convertedCoordinate);
-                firedPositions.Add(new Tuple<char, int>(typedCoordinates.ToUpper()[0], convertedCoordinate));
-                hasTargetBeenHit();
+
+                if (!string.IsNullOrWhiteSpace(typedCoordinates))
+                {
+                    Int32.TryParse(typedCoordinates.Substring(1), out int convertedCoordinate);
+                    _firedPositions.Add(new Tuple<char, int>(typedCoordinates.ToUpper()[0], convertedCoordinate));
+                    HasTargetBeenHit();
+                }
             }
             while (createdShips.Any(x => !x.IsSunk));
-            drawBattlefield(ref createdShips);
-            Console.WriteLine("You won! Press any key to countinue.");
+            DrawBattlefield(ref createdShips);
+            Console.WriteLine("You won! Press any key to continue.");
             Console.ReadKey();
         }
-        private bool isTypedCoordinateValid(string? typedCoordinates)
+        private bool IsTypedCoordinateValid(string? typedCoordinates)
         {
             if (string.IsNullOrWhiteSpace(typedCoordinates) || typedCoordinates.Length < 2
-                || !Char.IsLetter(typedCoordinates[0]) || !Int32.TryParse(typedCoordinates.Substring(1), out int result)) return false;
+                || !Char.IsLetter(typedCoordinates[0]) || !Int32.TryParse(typedCoordinates.Substring(1), out _)) return false;
             return true;
         }
-        private bool hasTargetBeenHit()
+        private void HasTargetBeenHit()
         {
-            var coordinates = firedPositions.Last();
-            if (createdShips.Any(x => x.Coordinates.Any(x => x.Item1 == coordinates.Item1 && x.Item2 == coordinates.Item2)))
+            var coordinates = _firedPositions.Last();
+            if (_createdShips.Any(ship => ship.Coordinates.Any(coordinate => coordinate.Item1 == coordinates.Item1 && coordinate.Item2 == coordinates.Item2)))
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Ship just has been hit!");                
-                if (hasShipSunk())
+                if (HasShipSunk())
                 {
                     Console.WriteLine("You have destroyed the ship!");
                     Console.WriteLine();
                 }
                 Console.ForegroundColor = ConsoleColor.Gray;
-                return true;
-
+                return;
             }
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Missed! Try again!");
             Console.ForegroundColor = ConsoleColor.White;
-            return false;
         }
-        private bool hasShipSunk()
+        private bool HasShipSunk()
         {
-            foreach (var ship in createdShips.Where(x => !x.IsSunk))
+            foreach (var ship in _createdShips.Where(x => !x.IsSunk))
             {
                 bool isSunk = true;
                 foreach (var coordinate in ship.Coordinates)
                 {
-                    if(!firedPositions.Any(x=>x.Item1 == coordinate.Item1 && x.Item2 == coordinate.Item2))
+                    if(!_firedPositions.Any(x=>x.Item1 == coordinate.Item1 && x.Item2 == coordinate.Item2))
                     {
                         isSunk = false;
                     }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
-using Battleship.Logic;
+using Battleship.Game;
 using Battleship.Model;
 
 const int numberOfRows = 10;
@@ -8,7 +8,7 @@ const int numberOfFiveSquaresShips = 1;
 const int numberOfFourSquaresShips = 2;
 List<Ship> createdShips = new List<Ship>();
 List<Tuple<char, int>> firedPositions = new List<Tuple<char, int>>();
-BattleshipLogic logic = new BattleshipLogic(numberOfFiveSquaresShips, numberOfFourSquaresShips, numberOfRows, numberOfColumns, 
+BattleshipGame logic = new BattleshipGame(numberOfFiveSquaresShips, numberOfFourSquaresShips, numberOfRows, numberOfColumns, 
     ref firedPositions, ref createdShips);
 logic.Init(ref createdShips);
 logic.Play(ref createdShips);
